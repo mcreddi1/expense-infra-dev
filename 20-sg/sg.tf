4201a633ec0724e241abef78484cabe7fc7fd66e
mcreddi1 <morachinnapareddy1996@gmail.com>
2024-09-28 19:32:56 +0530

    sg module terraform

--- 20-sg/sg.tf
+++ 20-sg/sg.tf
@@ -7,3 +7,31 @@ module "mysql_sg" {
    common_tags = var.common_tags
    sg_tags = var.mysql_sg_tags
 }
+
+module "backend_sg" {
+  source = "../../terraform-aws-sg"
+  environment = var.environment
+  project_name = var.project_name
+   sg_name = "backend"
+   vpc_id = local.vpc_id
+   common_tags = var.common_tags
+}
+
+module "frontend_sg" {
+  source = "../../terraform-aws-sg"
+  environment = var.environment
+  project_name = var.project_name
+   sg_name = "frontend"
+   vpc_id = local.vpc_id
+   common_tags = var.common_tags
+}
+
+#MYSQL Allowing connection on 3306 port from the instances attached to Backend SG 
+resource "aws_security_group_rule" "mysql_backend" {
+  type              = "ingress"
+  to_port           = 3306
+  from_port = 3306
+  protocol          = "tcp"
+  source_security_group_id = module.backend_sg.vpc_id
+  security_group_id = module.mysql_sg.id
+}
